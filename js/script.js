bc68516e548815cd7f67f4ab86c9f054fdc8a84c
Etrickery <ericspfister@gmail.com>
2015-02-10 22:18:00 -0500

    Minor tweaks
    Finishing the details in the comments

--- js/script.js
+++ js/script.js
@@ -11,13 +11,18 @@ This script is designed to accept user input representing a temperature,and then
 or the other way around. The user must first choose which direction to convert, and then input the requested temperature.
 The temperature is then converted and displayed in a concatenated statement within the console log.
 
-
+The user is prompted to select the direction, Fahrenheit to Celsius or Celsius to Fahrenheit. This is stored as true or
+false in the variable "choice", depending on the user's selection of "OK" or "CANCEL".
 
 The variables defined below are used to determine the desired conversion, and then output is displayed in the console
 using the result variable. The equations used in these calculations are as follows:
 
 C° = 5/9 * (F° - 32)
 F° = (9/5 * C°) + 32
+
+"tempConv" stores the results of these equations.
+
+"result" is the defined concatenated statement that details the user's desired results.
 
 
 
@@ -34,7 +39,7 @@ choice = confirm("If you would like to convert Fahrenheit to Celsius, click OK. 
 if (choice == true) {       //Fahrenheit to Celsius
 
     userInput = prompt("Please enter the temperature in Fahrenheit: ", 72);     //Request for value to be converted
-    
+
     tempConv = (Number(userInput) - 32) * (5 / 9);                              //Formula to convert the temperature
     tempConv = tempConv.toFixed(1);                                             //Needed to trim the decimal for aesthetics
     result = "Converting Fahrenheit to Celsius: " + (userInput) + "° Fahrenheit is equal to " + (tempConv) + "° Celsius.";
